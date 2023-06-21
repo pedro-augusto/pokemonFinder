11410e76463fe7ea288f910cadb2bff05274914f
Pedro Augusto Canteli <pcanteli@encamina.com>
2023-06-21 13:12:40 +0200

    finished project

--- js/app.js
+++ js/app.js
@@ -1,45 +1,69 @@
-    const cardArea = document.querySelector('#cardsArea');    
-    const form = document.querySelector('form');
+    // selectors
+    const cardArea = document.querySelector('#cardsArea');  
+    const form = document.querySelector('form'); 
     const pagination = document.querySelector('#pagination');
-    let url = `https://pokeapi.co/api/v2/pokemon/`;
-
     const forward = document.getElementById('forward');
     const goBack = document.getElementById('goBack');
 
-    window.onload = async () => {
+    let url = `https://pokeapi.co/api/v2/pokemon/`; // when nothing is searched, this is the default end-point. It returns 20 objects from the pokemon array and metadata about the array
+
+    async function getData(url){
+        const answer = await fetch(url);
+        const jsonPokemon = await answer.json();
+        return jsonPokemon;
+    }
+
+    window.onload = async () => { // when page is fully loaded
+
         form.addEventListener('submit', search);
+
         try {
+            let allHomeData= await getData(url); // getting all the pokemon data for the home page. Contains metadata about the pokemon array
+            let nextPage = allHomeData.next;
+            let previousPage = allHomeData.previous;
 
-            let allHomeData= await getData(url);
-            let previousPage;
+            pokemonsData = allHomeData.results; // this contains only the array of pokemons, no metadata
 
-            pokemonsData = allHomeData.results;
-            showPokemons(pokemonsData,"home");
+            showPokemons(pokemonsData,"home"); // sending only the array with pokemons to be shown
 
-            forward.addEventListener('click', async ()=>{
+            forward.addEventListener('click', async ()=>{ // acting when the user clicks to go to the next page
+                console.log(allHomeData.previous);
                 cleanCards();
-                url = allHomeData.next;
-                allHomeData= await getData(url);
-                pokemonsData= await getData(url);
-                previousPage = allHomeData.previous;
-                showPokemons(pokemonsData.results, "home");
+                url = nextPage;
+                allHomeData= await getData(url); // we need this to know what is the previous page
+                previousPage = allHomeData.previous; // updating previous page
+                nextPage= allHomeData.next; // updating next page
+                showPokemons(allHomeData.results, "home");
 
-                if(goBack.classList.contains("disabled") && allHomeData.previous!=null){
+                if(goBack.classList.contains("disabled") && allHomeData.previous!==null){ // manipulating when the button to go back is disabled
                     goBack.classList.remove("disabled");
                 }
+
+                else if(allHomeData.next==null){
+                    forward.classList.add("disabled");
+                }
+                
             })
 
             goBack.addEventListener('click', async ()=>{
-                url = previousPage;
                 cleanCards();
-                pokemonsData= await getData(url);
-                showPokemons(pokemonsData.results, "home");
+                url = previousPage; // updating the url to the link of the previous page
+                allHomeData= await getData(url);
+                nextPage = allHomeData.next;
+                previousPage= allHomeData.previous;
+                showPokemons(allHomeData.results, "home");
 
-                if(forward.classList.contains("disabled") && allHomeData.next!=null){
+                if(forward.classList.contains("disabled") && allHomeData.next!==null){ // manipulating when the button to go forward is disabled
                     forward.classList.remove("disabled");
+                }
+
+                else if(allHomeData.previous==null){
+                    goBack.classList.add("disabled");
                 }
             })
 
+
+            // disabling buttons when it is either the first or last page
             if(allHomeData.previous==null){
                 goBack.classList.add("disabled");
             }
@@ -49,63 +73,19 @@
             }
 
         } catch (error) {
-            console.log(error);
+            console.log(error); // FIXME: what is a more appropriate way to handle error?
         }
         
     }
 
-    async function search(e) {
-        cleanCards();
-        pagination.remove();
-
-        e.preventDefault();
-        const searchedTerm = document.querySelector('#term').value;
-        if(searchedTerm!='') {
-            try{ //FIXME: How to do this try catch thing in a better way? TRY CATCH
-                //searching by name
-                url = `https://pokeapi.co/api/v2/pokemon/${searchedTerm}`;
-                result = await getData(url);
-                generateCard(result);
-            } catch (error) {}
-
-            try {
-                //searching by ability
-                url = `https://pokeapi.co/api/v2/ability/${searchedTerm}`;
-                result = await getData(url);
-                showPokemons(result.pokemon,"ability");
-            } catch (error) {}
-
-            try {
-                //searching by type
-                url = `https://pokeapi.co/api/v2/type/${searchedTerm}`;
-                result = await getData(url);
-                showPokemons(result.pokemon,"type");
-            } catch (error) {}
-        }
-    }
-
-    function cleanCards(){
-        // cleaning previous results 
-        while(cardArea.firstChild){
-            cardArea.removeChild(cardArea.firstChild);
-        }
-    }
-
-    async function getData(url){
-        const answer = await fetch(url);
-        const jsonPokemon = await answer.json();
-        return jsonPokemon;
-    }
-
-    async function showPokemons(pokemonsData,type){
-        
+    async function showPokemons(pokemonsData,type){ // a type is sent because the structure of the array of pokemons used in the home page is different than the one used to search ability or type
+    
         let specificPokemonData;
 
-        pokemonsData.forEach(async function (pokemon) { 
+        pokemonsData.forEach(async function (pokemon) { // iterating each pokemon sent in the array (the array only returns the name and the link to the specific end-point of that pokemon)
             
-            //FIXME: IS THERE A BETTER WAY TO DO THIS WITHOUT NEEDING AN IF STATEMENT?
             if(type=="home"){
-                specificPokemonData = await getData(pokemon.url); // fetching data from the specific pokemon (accessing specific info through the previous array of objects);
+                specificPokemonData = await getData(pokemon.url); // fetching data from the specific pokemon (accessing specific info through the url attribute of each pokemon in the previous array of objects);
             } 
 
             else if (type=="ability" || type=="type"){
@@ -119,51 +99,88 @@
     }
 
     function generateCard(specificPokemonData){
-        console.log(specificPokemonData)
-        console.log(specificPokemonData.stats[0].base_stat)
 
         let card =` 
-
             <div class="card">
-            
                 <div class="image">
-                <img src="${specificPokemonData.sprites.front_default}">
+                    <img src="${specificPokemonData.sprites.front_default}">
                 </div>
                 <div class="content">
-                <div class="header capitalised">${specificPokemonData.name}</div>
-                <div class="meta">
-                    <a>Pokemon ID: ${specificPokemonData.id}</a>
-                </div>
-                <div class="description">` 
+                    <div class="header capitalised pokemonName">${specificPokemonData.name}</div>
+                    <div class="meta">Pokemon ID: ${specificPokemonData.id}</div>
+                    <div class="description">
+                        <div class="ui divider"></div>`; 
                     
-        specificPokemonData.stats.forEach(function (stat) {
-            card += `<div class="ui yellow progress" >
-            <div class="bar" style="transition-duration: 300ms; width: ${(stat.base_stat*100)/255}%;">
-                <div class="progress">${stat.base_stat}</div>
-            </div>
-            <div class="label">${(stat.stat.name).toUpperCase()}</div>
-            </div>`
-            
+        specificPokemonData.stats.forEach(function (stat) { // generating each progress bar
+            card+=`    <div class="ui yellow progress">
+                            <div class="bar" style="transition-duration: 300ms; width: ${(stat.base_stat*100)/255}%;">
+                                <div class="progress">${stat.base_stat}</div>
+                            </div>
+                            <div class="label">${(stat.stat.name).toUpperCase()}</div>
+                        </div>`;
         })        
-                
-            card+= `<div class="ui divider"></div><h4>Abilities:</h4>`;
+ 
+        card+=`        <div class="ui divider"></div>
+                        <h4 class="headerLabel">Abilities:</h4>`;
 
-            specificPokemonData.abilities.forEach(function (ability){
-                card+= `<span class="ui blue label capitalised center-aligned">${ability.ability.name}</span>`;
-            })
+        specificPokemonData.abilities.forEach(function (ability){ // generating each ability
+            card+= `    <span class="ui blue label capitalised center-aligned marginLabel">${ability.ability.name}</span>`;
+        })
                     
-            card +=`<div class="ui divider"></div>
-                        <h4>Type:</h4>`
+        card +=`        <div class="ui divider"></div>
+                        <h4 class="headerLabel">Type:</h4>`;
 
-            specificPokemonData.types.forEach( function (type){
-                card+= `<span class="ui red label capitalised center-aligned">${type.type.name}</span>`;
-            })
+        specificPokemonData.types.forEach( function (type){
+            card+= `    <span class="ui red label capitalised center-aligned marginLabel">${type.type.name}</span>`;
+        });
 
-            card+=`</div>
+        card+=`     </div>
                 </div>
             </div>`;
 
-            cardArea.innerHTML += card;
+            cardArea.innerHTML+= card;
+    }
+
+    function cleanCards(){
+        // cleaning previous results 
+        while(cardArea.firstChild){
+            cardArea.removeChild(cardArea.firstChild);
+        }
+    }
+
+    async function search(e) {
+        pagination.remove(); // when we are consulting the API for a specific pokemon, type or ability it does not return with pagination. Therefore, we delete the pagination in case the user searches something
+        e.preventDefault();
+
+        const searchedTerm = document.querySelector('#term').value;
+
+        if(searchedTerm!=='') {
+            cleanCards();
+
+            // FIXME: what is a better way to do this instead of 3 try catch?
+
+            try{
+                // searching by name
+                url = `https://pokeapi.co/api/v2/pokemon/${searchedTerm}`;
+                result = getData(url);
+                generateCard(result);
+            } catch (error) {}
+
+            try {
+                // searching by ability
+                url = `https://pokeapi.co/api/v2/ability/${searchedTerm}`;
+                result = await getData(url); // FIXME: wouldn't it be better and faster without async and await? For instance, if instead of waiting to check if what was searched was a name I could search everything at the same time
+                console.log(result.pokemon)
+                showPokemons(result.pokemon,"ability");
+            } catch (error) {}
+
+            try {
+                // searching by type
+                url = `https://pokeapi.co/api/v2/type/${searchedTerm}`;
+                result = await getData(url);
+                showPokemons(result.pokemon,"type");
+            } catch (error) {}
+        }
     }
 
     
