0b5083da82933b41e912f96f8c3a40b2107eb687
Pedro Augusto Canteli <pcanteli@encamina.com>
2023-06-23 12:54:05 +0200

    changes to the search function (filtering)

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@
     const pagination = document.querySelector('#pagination');
     const forward = document.getElementById('forward');
     const goBack = document.getElementById('goBack');
-
-    let url = `https://pokeapi.co/api/v2/pokemon/`; // when nothing is searched, this is the default end-point. It returns 20 objects from the pokemon array and metadata about the array
+    const errorArea = document.getElementById('error');
+    let url; // when nothing is searched, this is the default end-point. It returns 20 objects from the pokemon array and metadata about the array
 
     async function getData(url){
         const answer = await fetch(url);
@@ -14,10 +14,12 @@
     }
 
     window.onload = async () => { // when page is fully loaded
-
+        
         form.addEventListener('submit', search);
 
         try {
+            cleanCards();
+            url = `https://pokeapi.co/api/v2/pokemon/`;
             let allHomeData= await getData(url); // getting all the pokemon data for the home page. Contains metadata about the pokemon array
             let nextPage = allHomeData.next;
             let previousPage = allHomeData.previous;
@@ -42,7 +44,7 @@
                     forward.classList.add("disabled");
                 }
                 
-            })
+            });
 
             goBack.addEventListener('click', async ()=>{
                 cleanCards();
@@ -59,7 +61,7 @@
                 else if(allHomeData.previous==null){
                     goBack.classList.add("disabled");
                 }
-            })
+            });
 
 
             // disabling buttons when it is either the first or last page
@@ -72,7 +74,7 @@
             }
 
         } catch (error) {
-            console.log(error); // FIXME: what is a more appropriate way to handle error? // MESSAGE TO USER
+            showError("It was not possible to fetch data from the pokemon API") //
         }
         
     }
@@ -147,38 +149,107 @@
         }
     }
 
+    let foundSomething;
+
     async function search(e) {
+        clearErrors();
         pagination.remove(); // when we are consulting the API for a specific pokemon, type or ability it does not return with pagination. Therefore, we delete the pagination in case the user searches something
+        const nameChecked = document.querySelector('#name').checked;
+        const abilityChecked = document.querySelector('#ability').checked;
+        const typeChecked = document.querySelector('#type').checked;
+        let checked = document.querySelectorAll("#searchBar input:checked").length > 0;
+
         e.preventDefault();
+
+        if (!checked){
+            showError("You have to select at least one search type");
+        }
 
         const searchedTerm = document.querySelector('#term').value.toLowerCase();
 
         if(searchedTerm!=='') {
             cleanCards();
 
-            // FIXME: what is a better way to do this instead of 3 try catch? One function for each search
+            if(nameChecked==true){   
+                findPokemonByName(searchedTerm);
+            }
 
-            try{
-                // searching by name
-                url = `https://pokeapi.co/api/v2/pokemon/${searchedTerm}`;
-                result = await getData(url);
-                generateCard(result);
-            } catch (error) {}
+            if(abilityChecked==true){
+                findPokemonByAbility(searchedTerm);
+            }
 
-            try {
-                // searching by ability
-                url = `https://pokeapi.co/api/v2/ability/${searchedTerm}`;
-                result = await getData(url); // FIXME: wouldn't it be better and faster without async and await? For instance, if instead of waiting to check if what was searched was a name I could search everything at the same time
-                showPokemons(result.pokemon,"ability");
-            } catch (error) {}
-
-            try {
-                // searching by type
-                url = `https://pokeapi.co/api/v2/type/${searchedTerm}`;
-                result = await getData(url);
-                showPokemons(result.pokemon,"type");
-            } catch (error) {}
+            if(typeChecked==true){
+                findPokemonByType(searchedTerm);
+            }
         }
     }
+
+    async function findPokemonByName(searchedTerm){
+        try{
+            url = `https://pokeapi.co/api/v2/pokemon/${searchedTerm}`;
+            result = await getData(url);
+            generateCard(result);
+        } catch(error) {
+            showError('No pokemon by the name searched was found');
+        }
+    }
+
+    async function findPokemonByAbility(searchedTerm){
+        try{
+            url = `https://pokeapi.co/api/v2/ability/${searchedTerm}`;
+            result = await getData(url); // FIXME: wouldn't it be better and faster without async and await? For instance, if instead of waiting to check if what was searched was a name I could search everything at the same time
+            showPokemons(result.pokemon,"ability");
+        } catch(error) {
+            showError('No pokemon by the ability searched was found');
+        }
+    }
+
+    async function findPokemonByType(searchedTerm){
+        try{
+            url = `https://pokeapi.co/api/v2/type/${searchedTerm}`;
+            result = await getData(url);
+            showPokemons(result.pokemon,"type");
+        } catch(error){
+            showError('No pokemon by the type searched was found');
+        }
+    }
+
+    function showError(message){
+
+        errorArea.innerHTML +=
+        `
+        <div class="ui negative message">
+            <i class="close icon closeMessage"></i>
+            <div class="header">
+                There has been an error  :(
+            </div>
+            <p>${message}</p>
+        </div>`;
+
+        $('.closeMessage').on('click', function() {
+            $(this).closest('.message').transition('fade');
+        });
+
+    }
+
+    function clearErrors(){
+        while(errorArea.firstChild){
+            errorArea.removeChild(errorArea.firstChild);
+        }
+    }
+
+    $('.ui.accordion').accordion();
+
+    $("#frmTest").submit(function(){
+        var checked = $("#frmText input:checked").length > 0;
+        if (!checked){
+            alert("Please check at least one checkbox");
+            return false;
+        }
+    });
+
+
+
+
 
     
